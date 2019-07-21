fix bugs with masking out seen classes

eval.py
@@ -191,17 +191,17 @@ def evaluate(args):
     pointacc_list, pointacc_per_class_array, voxacc_list, voxacc_per_class_array, voxcaliacc_list, pointmiou_per_class_array, voxmiou_per_class_array, masks = eval_wholescene(args, model, dataloader)
     
     avg_pointacc = np.mean(pointacc_list)
-    avg_pointacc_per_class = np.mean(pointacc_per_class_array * masks, axis=0)
+    avg_pointacc_per_class = np.sum(pointacc_per_class_array * masks, axis=0)/np.sum(masks, axis=0)
 
     avg_voxacc = np.mean(voxacc_list)
-    avg_voxacc_per_class = np.mean(voxacc_per_class_array * masks, axis=0)
+    avg_voxacc_per_class = np.sum(voxacc_per_class_array * masks, axis=0)/np.sum(masks, axis=0)
 
     avg_voxcaliacc = np.mean(voxcaliacc_list)
     
-    avg_pointmiou_per_class = np.mean(pointmiou_per_class_array * masks, axis=0)
+    avg_pointmiou_per_class = np.sum(pointmiou_per_class_array * masks, axis=0)/np.sum(masks, axis=0)
     avg_pointmiou = np.mean(avg_pointmiou_per_class)
 
-    avg_voxmiou_per_class = np.mean(voxmiou_per_class_array * masks, axis=0)
+    avg_voxmiou_per_class = np.sum(voxmiou_per_class_array * masks, axis=0)/np.sum(masks, axis=0)
     avg_voxmiou = np.mean(avg_voxmiou_per_class)
 
     # report

lib/solver.py
@@ -185,7 +185,7 @@ class Solver():
             "iter_time": [],
             # loss (float, not torch.cuda.FloatTensor)
             "loss": [],
-            # constraint loss (float, not torch.cuda.FloatTensor)
+            # scores (float, not torch.cuda.FloatTensor)
             "point_acc": [],
             "point_acc_per_class": [],
             "voxel_acc": [],
@@ -256,7 +256,7 @@ class Solver():
             "iter_time": [],
             # loss (float, not torch.cuda.FloatTensor)
             "loss": [],
-            # constraint loss (float, not torch.cuda.FloatTensor)
+            # scores (float, not torch.cuda.FloatTensor)
             "point_acc": [],
             "point_acc_per_class": [],
             "voxel_acc": [],
@@ -334,11 +334,11 @@ class Solver():
         pointmiou, voxmiou, miou_mask = compute_miou(coords, preds, targets, weights)
         
         self._running_log["point_acc"] = pointacc
-        self._running_log["point_acc_per_class"] = np.mean(pointacc_per_class * acc_mask)
+        self._running_log["point_acc_per_class"] = np.sum(pointacc_per_class * acc_mask)/np.sum(acc_mask)
         self._running_log["voxel_acc"] = voxacc
-        self._running_log["voxel_acc_per_class"] = np.mean(voxacc_per_class * acc_mask)
-        self._running_log["point_miou"] = np.mean(pointmiou * miou_mask)
-        self._running_log["voxel_miou"] = np.mean(voxmiou * miou_mask)
+        self._running_log["voxel_acc_per_class"] = np.sum(voxacc_per_class * acc_mask)/np.sum(acc_mask)
+        self._running_log["point_miou"] = np.sum(pointmiou * miou_mask)/np.sum(miou_mask)
+        self._running_log["voxel_miou"] = np.sum(voxmiou * miou_mask)/np.sum(miou_mask)
 
     def _dump_log(self, epoch_id):
         # loss