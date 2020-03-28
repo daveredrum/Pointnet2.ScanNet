037411b519701f85957761814eae39f7908cb6ed
Dave Chen <davech2y@outlook.com>
2020-03-28 01:14:32 +0100

    add script to combine multiview features

--- scripts/combine_multiview_features.py
+++ scripts/combine_multiview_features.py
@@ -19,7 +19,8 @@ def get_scene_list():
 
 def load_scene(scene_list):
     scene_data = {}
-    for scene_id in scene_list:
+    print("loading data...")
+    for scene_id in tqdm(scene_list):
         scene_data[scene_id] = np.load(os.path.join(SCANNET_DATA, scene_id)+".npy")[:, :3]
 
     return scene_data
