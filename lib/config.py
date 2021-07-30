ddd833da391bf5fe3b46dab78972c543e03ff16c
Dave Z. Chen <davech2y@outlook.com>
2021-07-30 16:20:42 +0200

    add slurm scripts

--- lib/config.py
+++ lib/config.py
@@ -6,9 +6,18 @@ CONF = EasyDict()
 # BASE PATH
 CONF.ROOT = "/rhome/dchen/Pointnet2.ScanNet" # TODO change this
 CONF.SCANNET_DIR =  "/canis/Datasets/ScanNet/public/v2/scans" # TODO change this
-CONF.SCANNET_FRAMES_ROOT = os.path.join(CONF.ROOT, "frames_square")
-CONF.PROJECTION = os.path.join(CONF.ROOT, "multiview_projection_pointnet")
-CONF.ENET_FEATURES_ROOT = os.path.join(CONF.ROOT, "enet_features")
+
+# Uncomment the followings if you're NOT on slurm
+# CONF.SCANNET_FRAMES_ROOT = os.path.join(CONF.ROOT, "frames_square")
+# CONF.PROJECTION = os.path.join(CONF.ROOT, "multiview_projection_pointnet")
+# CONF.ENET_FEATURES_ROOT = os.path.join(CONF.ROOT, "enet_features")
+
+# Uncomment the followings if you're on slurm
+CONF.CLUSTER = "/cluster/balrog/dchen/Pointnet2.ScanNet"
+CONF.SCANNET_FRAMES_ROOT = os.path.join(CONF.CLUSTER, "frames_square")
+CONF.PROJECTION = os.path.join(CONF.CLUSTER, "multiview_projection_pointnet")
+CONF.ENET_FEATURES_ROOT = os.path.join(CONF.CLUSTER, "enet_features")
+
 CONF.ENET_FEATURES_SUBROOT = os.path.join(CONF.ENET_FEATURES_ROOT, "{}") # scene_id
 CONF.ENET_FEATURES_PATH = os.path.join(CONF.ENET_FEATURES_SUBROOT, "{}.npy") # frame_id
 CONF.SCANNET_FRAMES = os.path.join(CONF.SCANNET_FRAMES_ROOT, "{}/{}") # scene_id, mode 
